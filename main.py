0e60269ae4d258925c1df144c300074eab9a5c17
Tarkan Batar <tarkanbatar@MacBook-Pro.local>
2021-02-19 17:59:41 +0300

    feat: completed

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ fileContent = []
 t = 1
 
 def pageScroller(a):
-    while(time.time() - a < 10):
+    while(time.time() - a < 180):
         html = driver.find_element_by_tag_name('html')
-        html.send_keys(Keys.END)
-        html.send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        for w in range(45):
+            html.send_keys(Keys.ARROW_DOWN)
+        html.send_keys(Keys.PAGE_DOWN)
+        for q in range(45):
+            html.send_keys(Keys.ARROW_UP)
 
 def fileOpener(k):
     textFile = open("/Users/tarkanbatar/Desktop/tarkan/%d.txt" % k, "r")
@@ -29,46 +33,48 @@ for i in range(fileNumber):
     fileContent.append(fileOpener(t))
     t += 1
 
-for n in range(fileNumber):
-    random.shuffle(fileContent[n])
-    print(fileContent[n])
+while True:
+    for n in range(fileNumber):
+        random.shuffle(fileContent[n])
+        print(fileContent[n])
 
 
-options = webdriver.ChromeOptions()
-options.add_extension('./cknebhggccemgcnbidipinkifmmegdel.crx')
-driver = webdriver.Chrome(options=options)
-driver.get("chrome-extension://cknebhggccemgcnbidipinkifmmegdel/html/welcome.html")
-time.sleep(3)
+    options = webdriver.ChromeOptions()
+    options.add_extension('./cknebhggccemgcnbidipinkifmmegdel.crx')
+    driver = webdriver.Chrome(options=options)
+    driver.get("chrome-extension://cknebhggccemgcnbidipinkifmmegdel/html/welcome.html")
+    time.sleep(3)
 
-extensionAccept = driver.find_element_by_id("accept")
-driver.window_handles[0]
-extensionAccept.click()
-time.sleep(3)
+    extensionAccept = driver.find_element_by_id("accept")
+    driver.window_handles[0]
+    extensionAccept.click()
+    time.sleep(3)
 
-driver.window_handles[1]
-driver.switch_to_window(driver.window_handles[0])
-driver.get(fileContent[0][0])
+    driver.window_handles[1]
+    driver.switch_to_window(driver.window_handles[0])
+    driver.get(fileContent[0][0])
 
-for k in range(fileNumber - 2):
-    driver.execute_script("window.open('');")
-    driver.window_handles[k + 2]
-    driver.switch_to_window(driver.window_handles[k + 2])
+    for k in range(fileNumber - 2):
+        driver.execute_script("window.open('');")
+        driver.window_handles[k + 2]
+        driver.switch_to_window(driver.window_handles[k + 2])
 
 
-for p in range(fileNumber):
-    lineNum = len(fileContent[p])
+    for p in range(fileNumber):
+        lineNum = len(fileContent[p])
 
-    for b in range(lineNum):
-        for t in range(fileNumber):
-            driver.switch_to_window(driver.window_handles[t])
-            time.sleep(1)
-            driver.get(fileContent[t][b])
-            time.sleep(2)
+        for b in range(lineNum):
+            for t in range(fileNumber):
+                driver.switch_to_window(driver.window_handles[t])
+                time.sleep(1)
+                driver.get(fileContent[t][b])
+                time.sleep(2)
 
-        for s in range(fileNumber):
-            driver.switch_to_window(driver.window_handles[s])
-            time_start = time.time()
-            pageScroller(time_start)
+            for s in range(fileNumber):
+                driver.switch_to_window(driver.window_handles[s])
+                time.sleep(2)
+                time_start = time.time()
+                pageScroller(time_start)
 
 
 
